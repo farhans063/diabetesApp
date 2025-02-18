34317e0d9ef34ee14f54aa93251b78a758bd9722
farhans063 <fs10893@outlook.com>
2025-02-18 23:59:14 +0000

    TextInput in CalendarScreen (#3)
    TextInput created which iterates over 'allReadings' array + more code refactorisation + more styling + logo

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,29 +1,49 @@
-import { StyleSheet, ScrollView } from "react-native";
+import { StyleSheet, Image, Text, ScrollView } from "react-native";
 import Screen from "../layout/Screen";
 import HomeScreenItem from "../entity/HomeScreenItem.js";
 
 const HomeScreen = ({ navigation }) => {
   // Initialisations
   // State
-
   // Handlers
-  const goToCalendarScreen = () => navigation.navigate("CalendarScreen");
-  const goToEducationScreen = () => navigation.navigate("EducationScreen");
-
   // View
   return (
     <Screen>
-      <ScrollView style={styles.container}>
-        <HomeScreenItem title={"Calendar"} onSelect={goToCalendarScreen} />
+      <ScrollView>
+        <Image
+          source={require("../../data/DiabetesTrackerLogo4.jpg")}
+          style={styles.image}
+        />
+        <Text style={styles.subtitle}>
+          Here to help you manage your diabetes a little better, everyday!
+        </Text>
+        <HomeScreenItem
+          title={"Calendar"}
+          onSelect={() => navigation.navigate("CalendarScreen")}
+        />
         <HomeScreenItem
           title={"Learn about Diabetes"}
-          onSelect={goToEducationScreen}
+          onSelect={() => navigation.navigate("EducationScreen")}
         />
       </ScrollView>
     </Screen>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    marginTop: 20,
+    marginBottom: 10,
+    width: 320,
+    height: 40,
+    resizeMode: "contain",
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: "800",
+    color: "grey",
+    marginBottom: 25,
+  },
+});
 
 export default HomeScreen;
